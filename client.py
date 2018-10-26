922773e03073e01ce6b2c513da97a25088fe1f65
NachIbanez <43443842+NachIbanez@users.noreply.github.com>
2018-10-26 14:47:32 +0200

    Ejercicio 8 hecho (.json y time)

--- client.py
+++ client.py
@@ -6,6 +6,7 @@ Programa cliente UDP que abre un socket a un servidor
 
 import socket
 import sys
+import time
 
 try:
     SERVER = sys.argv[1]
@@ -14,18 +15,17 @@ try:
     DIR_SIP = sys.argv[4]
     EXPIRES = sys.argv[5]
 except IndexError:
-    sys.exit("Usage: client.py port register sip_address expires_value")
+    sys.exit("Usage: client.py ip port register sip_address expires_value")
 
 
 if METHOD == "register":
 
-    LINE = ("REGISTER sip:" + DIR_SIP + " SIP/2.0\r\n" + "Expires: " + str(EXPIRES) + "\r\n")
+    LINE = ("REGISTER " + DIR_SIP + " SIP/2.0\r\n" + "Expires: " + str(EXPIRES) + "\r\n")
     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
         my_socket.connect((SERVER, PORT))
         my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
         data = my_socket.recv(1024)
         print('Recibido -- ', data.decode('utf-8'), end="")
-
 else:
     LINE = " ".join(sys.argv[3:])
 
--- server.py
+++ server.py
@@ -3,28 +3,55 @@
 
 import socketserver
 import sys
+import time
 
 
 class SIPRegisterHandler(socketserver.DatagramRequestHandler):
 
+    def register2json(self, diccionario_registro):
+        usuarios = list(diccionario_registro)
+        for usuario in usuarios:
+            gmt_expires = diccionario_registro[usuario][1]
+            gmt_actual = (time.strftime(' GMT %Y-%m-%d %H:%M:%S', time.gmtime(time.time())))
+            if gmt_expires < gmt_actual:
+                del diccionario_registro[usuario]
+        file = open("registered.json", "w")
+        file.write("{" + '\n\t"Registered Users"' + ": {\n\t\t")
+        ultima_iteracion = len(diccionario_registro)
+        iteracion = 1
+        for usuario in diccionario_registro:
+            file.write('\n\t\t"' + usuario + ' "' + ": {\n\t\t\t")
+            lista = diccionario_registro[usuario]
+            file.write('"address": ' + '"' + lista[0] + '",' + "\n\t\t\t")
+            file.write('"expires": ' + '"' + lista[1] + '"')
+            if ultima_iteracion == iteracion:
+                file.write("\n\t\t}")
+            else:
+                file.write("\n\t\t},")
+            iteracion += 1
+        file.write("\n\t}\n}")
 
     def handle(self):
-
         for line in self.rfile:
             line_decoded = line.decode('utf-8')
             if line_decoded[:line_decoded.find(" ")] == "REGISTER":
                 direccion_sip = line_decoded[line_decoded.find(" "):line_decoded.rfind(" ")]
                 diccionario_registro[direccion_sip] = \
-                    self.client_address[0]
+                    [self.client_address[0]]
                 self.wfile.write(b'SIP/2.0 200 OK\r\n\r\n')
                 self.host, self.port = self.client_address[:2]
                 print("--" + line.decode('utf-8'), end="")
                 print("Desde ip:puerto --> " + str(self.host) + ":" + str(self.port), "\n")
             elif line_decoded[:line_decoded.find(" ")] == "Expires:":
                 expires = line_decoded[line_decoded.find(" "):][1:].replace(" ", "")
+                gmt_expires = time.strftime(" GMT %Y-%m-%d %H:%M:%S", \
+                                            time.gmtime(time.time() \
+                                            + int(expires)))        
+                expires_seconds_gmt = time.gmtime(time.time() + int(expires))
+                diccionario_registro[direccion_sip].append(gmt_expires)
                 if int(expires) == 0:
-                    del diccionario_registro[direccion_sip]
-        print(diccionario_registro)
+                    del diccionario_registro[direccion_sip]       
+        SIPRegisterHandler.register2json(self, diccionario_registro)
 
 
 if __name__ == "__main__":
